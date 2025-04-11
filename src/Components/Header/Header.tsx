6646ba37d208d2d1831cb2e9a34113f6698df85a
Nick <144842765+Nexexexexed@users.noreply.github.com>
2025-04-11 12:57:27 +0300

    adding navbar and sidebar

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,75 @@
+import styles from "./Header.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { useState, useRef } from "react";
+
+const Header = () => {
+  const tabs = [
+    "Свод данных по сотрудникам",
+    "Сводный отчет внутри компании",
+    "Сводный отчет по сделкам",
+  ];
+  const [activeTab, setActiveTab] = useState(1);
+  const tabRefs = useRef<(HTMLSpanElement | null)[]>([]);
+
+  const handlePrev = () => {
+    setActiveTab((prev) => (prev - 1 + tabs.length) % tabs.length);
+  };
+
+  const handleNext = () => {
+    setActiveTab((prev) => (prev + 1) % tabs.length);
+  };
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.header_nav}>
+        <button onClick={handlePrev}>
+          <FontAwesomeIcon icon={faChevronLeft} />
+        </button>
+        <button onClick={handleNext}>
+          <FontAwesomeIcon icon={faChevronRight} />
+        </button>
+      </div>
+
+      <div className={styles.header_tabs}>
+        <div className={styles.tab_container}>
+          {tabs.map((tab, index) => (
+            <span
+              key={index}
+              ref={(el) => {
+                tabRefs.current[index] = el;
+              }}
+              className={`${styles.tab} ${
+                activeTab === index ? styles.active : ""
+              }`}
+            >
+              {tab}
+            </span>
+          ))}
+          <div
+            className={styles.underline}
+            style={{
+              width: tabRefs.current[activeTab]?.offsetWidth || 0,
+              left: tabRefs.current[activeTab]?.offsetLeft || 0,
+            }}
+          />
+        </div>
+      </div>
+
+      <div className={styles.header_profile}>
+        <div className={styles.header_avatar}>
+          <img src="/avatar.png" alt="avatar" />
+        </div>
+        <div className={styles.text}>
+          <div className={styles.nickname}>Kristina 🐰</div>
+          <div className={styles.post}>менеджер продаж</div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Dashboard from "./Components/Dashboard/index";
+
+import App from "./App";
 import "./main.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Dashboard />
+    <App />
   </StrictMode>
 );
